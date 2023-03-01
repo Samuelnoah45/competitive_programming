b3ed4f036dfdeb8548c98614f5d33a2e9f91469a
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-01 11:16:46 +0300

    added minimum-size-subarray-sum.python

--- minimum-size-subarray-sum.py
+++ minimum-size-subarray-sum.py
@@ -7,16 +7,23 @@ class Solution:
         '''
 
         l = 0
+        r = 0
         ans = len(nums) + 1
         sums = 0
         
-        for r in range(len(nums)):
-            sums+=nums[r]
 
-            while sums>=target:
-                ans = min(ans, r-l+1)
-                sums-=nums[l]
-                l+=1
-            
-        
-        return 0 if ans==len(nums)+1 else ans
+        while r < len(nums):
+            sums += nums[r]
+            if sums < target:
+                r += 1
+            else:
+                while  sums >= target :
+                    ans = min(ans,r-l+1) 
+                    sums -= nums[l]
+                    if l == r:
+                        return 1
+                    l += 1
+                r += 1
+
+                
+        return 0  if  ans == len(nums) + 1 else ans
