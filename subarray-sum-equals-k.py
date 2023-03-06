17ae4d3972e0de602c07c324be9b55ee3e90b3c4
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-06 14:40:03 +0300

    added subarray-sum-equals-k.python

--- subarray-sum-equals-k.py
+++ subarray-sum-equals-k.py
@@ -1,16 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        sums = 0
-        res = 0
-        hash = {0:1}
-        for num in nums:
-            sums += num
-            if (sums -k) not in hash:
-                 hash[sums] = hash.get(sums,0) + 1
+        hash = defaultdict(int)
+        hash[0] = 1
+        count = 0
+        
+        prefix = list(accumulate(nums))
+    
+        for num in prefix:
+            if num - k in hash:
+                count += hash[num - k]
+            hash[num] += 1
 
-            else:
-                res += hash[sums-k]
-                hash[sums] = hash.get(sums,0) + 1
-
-            
-        return res
+        return count
