37dc66c220e12ab07fbfb46c5a8e0f0814aafabd
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-07 09:34:19 +0300

    Time: 27 ms (91.40%), Space: 13.9 MB (50.83%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -1,21 +1,14 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
 
-        ans = []
         if rowIndex == 0:
             return [1]
-        for i in range(rowIndex +1):
-            if i == 0:
-                ans = [1]
-            if i == 1:
-                ans = [1,1]
-            else:
-                temp = []
-                for i in range(len(ans) -1):
-                    temp.append( ans[i] + ans[ i+1 ] )
-                    print(ans)
-                ans = [1] + temp + [1]
+        else:
+            prevArr = self.getRow(rowIndex -1)
+            temp = []
+            for i in range(len(prevArr) -1):
+                temp.append(prevArr[i] + prevArr[i +1])
+                
+            return  [1] + temp + [1]
 
-
-
-        return ans
+        
