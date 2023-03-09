5b922455040845b4df658182c97ac1708fa64925
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-09 16:41:24 +0300

    added validate-binary-search-tree.python

---
+++ validate-binary-search-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def validation(root):
+            arr = []
+            if root == None:
+                return  []
+        
+            arr.extend(validation(root.left))
+            arr.append(root.val)
+            arr.extend(validation(root.right))
+            return arr
+
+        newArr = validation(root)
+        for i in range(len(newArr)-1):
+            if newArr[i] >= newArr[i+1]:
+                return False
+
+        return True
