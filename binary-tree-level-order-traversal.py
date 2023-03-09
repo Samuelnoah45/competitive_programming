7aa4e7053474a5bc6d1177e3eb15bec7ec3c546b
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-09 19:31:56 +0300

    added binary-tree-level-order-traversal.python

---
+++ binary-tree-level-order-traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        hash=  defaultdict(list)
+        def levelSearch(k,root):
+
+            if not root:
+                return 
+            hash[k].append(root.val)
+            l =levelSearch(k+1, root.left) 
+            r = levelSearch(k+1, root.right) 
+        
+
+
+        levelSearch( 0,root)
+        return list(hash.values())
