8c7fbedd589e09cf3c5c3fc0fc639eb7f4fcbbcb
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-16 17:50:31 +0300

    Time: 9346 ms (5.13%), Space: 13.9 MB (75.66%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -0,0 +1,18 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+
+        def predict(l ,r , t ):
+
+            if l > r:
+                return 0
+            
+            if t:
+                return max(nums[l] + predict(l+1 , r , not t) ,nums[r]+ predict(l , r -1 , not t) )
+            else:
+                return min(-nums[l]+ predict(l + 1 , r, not t) , -nums[r]+ predict(l , r - 1, not t) )
+        return predict(0 , len(nums) - 1 , True) >= 0 
+        
+        
+        
+
+           
