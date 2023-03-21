9706bd0bb5f09cf98bb800d18c14e6deefce777f
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-22 01:03:05 +0300

    added koko-eating-bananas.python

---
+++ koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution:
+        def minEatingSpeed(self, piles: List[int], H: int) -> int:
+            
+            maxVal = max(piles)+1
+            minVal  = 0
+            result = maxVal
+            while minVal + 1 < maxVal:
+                mid =( maxVal + minVal) //2
+                count = 0
+                for ban in piles:
+                    count += ceil(ban/mid)
+                    
+                if count <= H:
+                    maxVal = mid
+                    
+                else:
+                    minVal = mid
+            
+            return maxVal
