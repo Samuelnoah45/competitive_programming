498405219583f4e59aa1e4956791e1562f6e9e00
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-03-28 14:39:30 +0300

    added middle-of-the-linked-list.python

---
+++ middle-of-the-linked-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        slow = head
+        fast = head
+
+        while fast != None: 
+            if fast.next == None:
+                fast = fast.next
+                # break
+            else:
+                fast = fast.next.next
+                slow = slow.next
+
+        return slow
