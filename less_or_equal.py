62fa6d7472c0b97b7057202da17cece860eaa4b3
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-04-02 10:20:50 +0300

    added less_or_equal.py

---
+++ less_or_equal.py
@@ -0,0 +1,15 @@
+n , k = list(map(int, input().split()))
+
+nums = list(map(int , input().split()))
+nums.sort()
+if n == k:
+    print(nums[k-1])
+elif k == 0:
+    if nums[0] > 1:
+        print(1)
+    else:
+        print(-1)
+elif nums[k] != nums[k-1]:
+    print(nums[k-1])
+elif nums[k] == nums[k-1]:
+    print(-1)
