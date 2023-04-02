3909a9cab284e8999730ae88d9c174a150d3a288
Samuel Naoh <86590161+Samuelnoah45@users.noreply.github.com>
2023-04-02 09:43:28 +0300

    added isSorted.py

---
+++ isSorted.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+class Solution:
+    def arraySortedOrNot(self, arr, n):
+        # code here
+        l = 0
+        r = 1
+        while r < n:
+            
+            if arr[l] > arr[r]:
+                
+                return False
+                
+            r += 1
+            l += 1
+        return True
